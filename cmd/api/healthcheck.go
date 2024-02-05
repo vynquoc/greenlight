11269c7dd43d7c6dcecfb9843011e5bd7926179b
vynquoc <vynquoc@gmail.com>
2024-02-05 19:21:10 +0700

    declare movie api and define data type

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) healcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem", http.StatusInternalServerError)
+	}
+}
---
+++ cmd/api/movies.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/vynquoc/greenlight/internal/data"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create new")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Interstellar",
+		Runtime:   102,
+		Genres:    []string{"fiction", "cosmos"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem", http.StatusInternalServerError)
+	}
+}
